69338c4b0ceb9dbafda266771418d24d3cc7d915
antoninovaccarella <antoninovaccarella@outlook.com>
2023-06-05 13:04:46 +0200

    Update WebSecurityConfig.java

--- src/main/java/com/antonino/book101server/security/config/WebSecurityConfig.java
+++ src/main/java/com/antonino/book101server/security/config/WebSecurityConfig.java
@@ -68,16 +68,4 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"));
-        configuration.setAllowedHeaders(Arrays.asList("X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization"));
-        configuration.setAllowCredentials(true);
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
-
 }
